2637be9ed41c1cc068e35c9e1561bc14ecbefce2
XPrimerX <spakantonpetr@gmail.com>
2026-09-14 14:11:10 +0300

    First try

---
+++ Labs_1/Lab_1/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using Lab_1.BD;
+using Lab_1.Interface;
+using Lab_1.Services;
+using Lab_1.Services.Parse_File;
+
+
+namespace Lab_1;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        base.OnStartup(e);
+
+        var dbContext = new AppDbContext("app.db");
+        dbContext.Database.EnsureCreated();
+
+        IDataReader dataReader = new FileReader();
+        ITransactionValidator validator = new TransactionValidator();
+        TransactionBD repository = new TransactionBD(dbContext);
+        var progressReporter = new ViewModelProgressReporter();
+        IImportSummaryWriter summaryWriter = new ConsoleSummaryWriter();
+
+        var parserResolver = new ParserResolver(new Dictionary<string, ITransactionParser>
+        {
+            [".csv"] = new CsvTransactionParser(),
+            [".json"] = new JsonTransactionParser(),
+            [".xml"] = new XmlTransactionParser()
+        });
+
+        var viewModel = new MainViewModel(
+            parserResolver, dataReader, validator, repository, progressReporter, summaryWriter);
+
+        var window = new MainWindow { DataContext = viewModel };
+        window.Show();
+    }
+}
---
+++ Labs_1/Lab_1/Services/ParserResolver.cs
@@ -0,0 +1,23 @@
+﻿using Lab_1.Interface;
+
+namespace Lab_1.Services;
+
+public class ParserResolver
+{
+    private readonly Dictionary<string, ITransactionParser> _parsers;
+
+    public ParserResolver(Dictionary<string, ITransactionParser> parsers)
+    {
+        _parsers = parsers;
+    }
+
+    public ITransactionParser Resolve(string extension)
+    {
+        var key = extension.ToLowerInvariant();
+
+        if (!_parsers.TryGetValue(key, out var parser))
+            throw new NotSupportedException($"Формат файлу не підтримується: {extension}");
+
+        return parser;
+    }
+}
